917f58ecd7b48205291a37e579875d6a5a36f28c
PBDm <pbdm915@gmail.com>
2013-03-26 01:49:24 +0100

    添加google跟踪代码,更改中文版chrome插件cv内容

--- public/protected/pbdm/Lang/zh/common.php
+++ public/protected/pbdm/Lang/zh/common.php
@@ -50,7 +50,7 @@ return array(
     'cv_scade'=>'用Scade写的同步编程语言 (Lustre 和 Scade) 的项目',
     'cv_android'=>'安卓应用的开发 (一个记事本)',
     'cv_iphone'=>'IPHONE应用的开发',
-    'cv_chrome'=>'Chrome应用的开发',
+    'cv_chrome'=>'Chrome应用的开发 (一个小的汇率转换工具.利用AJAX读取数据并同步显示)',
     'cv_uml'=>'用Java实现的一个UML图编辑器和一个代码生成器',
     'cv_pulls'=>'先前可行性研究进口中国制造的羊绒衫',
 
