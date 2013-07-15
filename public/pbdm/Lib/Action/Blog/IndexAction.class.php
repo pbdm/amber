343a21492fbc868b7c42f88066fc419e0c1dc7c0
PBDm <pbdm915@gmail.com>
2013-07-15 23:49:38 +0800

    修复不能使用中文名做文章名bug

--- public/pbdm/Lib/Action/Blog/IndexAction.class.php
+++ public/pbdm/Lib/Action/Blog/IndexAction.class.php
@@ -6,16 +6,15 @@ class IndexAction extends MyAction {
         $this->assign('title', L('blog'));
         
         $files = dirtree(C("PATH_ACTICLE"));
-        $url = to_utf($url);
 
-        $fullname = $url.C("TMPL_TEMPLATE_SUFFIX");
+        $fullname = to_utf($url.C("TMPL_TEMPLATE_SUFFIX"));
 
         foreach($files as &$file){
+            $acticle['path'] = C("PATH_ACTICLE").$file;
             $file = to_utf($file);
             $acticle['name'] = substr($file,11,-5);
             $acticle['date'] = substr($file,0,10);
             $acticle['fullname'] = $file;
-            $acticle['path'] = C("PATH_ACTICLE").$file;
             if(!strcasecmp($fullname,$file)){
                $act = $acticle;//让文件名上的名字为文章的名字,(文件名上的名字是大小写敏感的)
             }
