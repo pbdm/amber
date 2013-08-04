df7e17a08529c833e5999092dfd9ab31136bbaa4
PBDm <pbdm915@gmail.com>
2013-08-04 19:47:28 +0800

    删除Thinkphp时代文件

--- public/public/js/docs.js
+++
@@ -1,8 +0,0 @@
-//为Table 添加样式
-$('.cv table').addClass('table table-hover');
-
-$(document).ready(function(){
-    $('.wiki a').each(function(){
-        $(this).attr('target','_blank');   
-    });  
-});
