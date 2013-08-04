a6639fed68a24599c1dde9a9126b981f67bb2a97
PBDm <pbdm915@gmail.com>
2013-08-04 20:18:04 +0800

    change entrance to folder public

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
-define('BASE_PATH',dirname(__FILE__).DS);
+define('BASE_PATH',trim($_SERVER['DOCUMENT_ROOT'],'public'));
 define('AMBER_PATH',BASE_PATH.'Amber'.DS);
 define('APP_PATH',BASE_PATH.'pbdm'.DS);
 require(AMBER_PATH.'Amber.php');
