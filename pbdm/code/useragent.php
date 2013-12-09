fa2b7178d112c3fe6fe7bcf0ec4247d09dfb235d
PBDm <pbdm915@gmail.com>
2013-12-09 22:43:31 +0800

    Edit small pieces and drupal sumarry

---
+++ pbdm/code/useragent.php
@@ -0,0 +1,14 @@
+<?php
+strstr($_SERVER["HTTP_USER_AGENT"], "MSIE 8.0");
+strstr($_SERVER["HTTP_USER_AGENT"], "MSIE 7.0");
+strstr($_SERVER["HTTP_USER_AGENT"], "MSIE 6.0");
+strstr($_SERVER["HTTP_USER_AGENT"], "NetCaptor");
+strstr($_SERVER["HTTP_USER_AGENT"], "Netscape");
+strstr($_SERVER["HTTP_USER_AGENT"], "Lynx");
+strstr($_SERVER["HTTP_USER_AGENT"], "Opera");
+strstr($_SERVER["HTTP_USER_AGENT"], "Konqueror");
+strstr($_SERVER["HTTP_USER_AGENT"], "Mozilla/5.0");
+strstr($_SERVER["HTTP_USER_AGENT"], "Firefox");
+strstr($_SERVER["HTTP_USER_AGENT"], "Firefox/3");
+strstr($_SERVER["HTTP_USER_AGENT"], "Firefox/2"); 
+strstr($_SERVER["HTTP_USER_AGENT"], "Chrome");
