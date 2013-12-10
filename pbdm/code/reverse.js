90cd1ffcd37b6015c677251c459ccab6f74ea567
PBDm <pbdm915@gmail.com>
2013-12-11 00:00:05 +0800

    Small edit for code reverse.js

--- pbdm/code/reverse.js
+++ pbdm/code/reverse.js
@@ -3,7 +3,7 @@
 	document.write("<pre>"+arr+"</pre>");
 	arr = arrrev(arr);
 	arr = wordrev(arr);
-	function arrrev(arr){
+	function arrrev(arr) {
 		var len = arr.length;
 		var newarr = '';
 		for (var i = len-1 ; i >= 0 ; i-- ) {
@@ -11,25 +11,25 @@
 		}
 		return newarr;
 	}
-	function wordrev($arr){
-        var len = arr.length;
-        var newarr = '';
-        var word = '';
-        for(var i = 0 ; i < len ; i++) {
-        	if (arr[i] != ' ') {
-        		word += arr[i];
-        	} else {
-        		word = arrrev(word);
-        		newarr += word;
-        		newarr +=' ';
-        		word = '';
-        	}
-        	if(i == len-1) { //for the last word
-        		word = arrrev(word);
-        		newarr += word;
-        	}   	
-        }
-        return newarr;
-    }
+	function wordrev($arr) {
+		var len = arr.length;
+		var newarr = '';
+		var word = '';
+		for(var i = 0 ; i < len ; i++) {
+			if (arr[i] != ' ') {
+				word += arr[i];
+			} else {
+				word = arrrev(word);
+				newarr += word;
+				newarr +=' ';
+				word = '';
+			}
+			if(i == len-1) { //for the last word
+				word = arrrev(word);
+				newarr += word;
+			}   	
+		}
+		return newarr;
+	}
 	document.write("<pre>"+arr+"</pre>");
 </script>
