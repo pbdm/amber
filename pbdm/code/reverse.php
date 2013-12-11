a068c061c6a29782feebab8827c7beb204580f24
PBDm <pbdm915@gmail.com>
2013-12-11 23:26:33 +0800

    Edit acticle 按照单词反转字符串

--- pbdm/code/reverse.php
+++ pbdm/code/reverse.php
@@ -8,12 +8,42 @@ class reverse{
     }
 
     private function reverse($arr){
-        $arr = $this -> arrrev($arr);
-        $arr = $this -> wordrev($arr);
+        $r = $this -> arrrevs($arr);
+        $arr = $r[0];
+        $r[1] = $this -> arrrev($r[1]);
+
+        $arr = $this -> wordrev($arr,$r[1]);
         return $arr;
     }
 
 	/**
+ 	 * 将字符串反转 , 并取得空格数目数组
+ 	 */
+    private function arrrevs($arr){
+        $len = strlen($arr);
+        $newarr = '';
+        $arrblank = '';
+        $num_blank = 0;
+        $last_str = true;
+        for($i = $len-1 ; $i >= 0 ; $i--) {
+        	$newarr .= $arr[$i];
+        	if ($arr[$i] ==' ') {
+        		$last_str = false;
+        		$num_blank ++;
+        	} else {
+        		if (!$last_str) { //last str is blank
+        			$arrblank .= $num_blank;
+        			$num_blank = 0;
+        		}
+        		$last_str = true;
+        	}
+        }
+       
+        $r = array($newarr,$arrblank);
+        return $r;
+    }
+
+    /**
  	 * 将字符串反转
  	 */
     private function arrrev($arr){
@@ -28,18 +58,27 @@ class reverse{
     /**
      * 将字符串里的每个单词反转
      */
-    private function wordrev($arr){
+    private function wordrev($arr,$arrblank){
         $len = strlen($arr);
         $newarr = '';
         $word = '';
+        $last_str = true;
+        $j = 0;
         for($i = 0 ; $i < $len ; $i++) {
         	if ($arr[$i] != ' ') {
+        		if (!$last_str) {//last str is blank
+        			$newarr .= $this -> getblank($arrblank,$j);
+        			$j ++;
+        		}
         		$word .= $arr[$i];
+        		$last_str = true;
         	} else {
-        		$word = $this -> arrrev($word);	
-        		$newarr .= $word;
-        		$newarr .=' ';
-        		$word = '';
+        		if ($last_str) {
+        			$word = $this -> arrrev($word);	
+        			$newarr .= $word;
+        			$word = '';
+        		}	
+        		$last_str = false;
         	}
         	if($i == $len-1) { //for the last word
         		$word = $this -> arrrev($word);
@@ -47,6 +86,15 @@ class reverse{
         	}   	
         }
         return $newarr;
+    }
+
+    private function getblank($arrblank,$num){
+    	$len = $arrblank[$num];
+    	$r = '';
+    	for($i = 0; $i < $len ; $i++) {
+    		$r .= ' ';
+    	}
+    	return $r;
     }
 }
  
