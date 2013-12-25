be7c989d5c2fe7ae07638501a974347142d492fc
PBDm <pbdm915@gmail.com>
2013-12-25 20:37:00 +0800

    Change to origin js compiler for less

--- less.bat
+++ less.bat
@@ -1,5 +1,5 @@
 @echo off
 
-lessc --source-map-map-inline public/override.less > public/style.css
-
+::lessc --source-map-map-inline public/override.less > public/style.css
+recess public/override.less --compress > public/style.css
 pause
